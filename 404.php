a3c1412ef4e4d429f1a7e65be5e70940b84760f4
romain Leboiteux <90255831+romainflly76@users.noreply.github.com>
2021-12-21 21:40:54 +0100

    Page 404

---
+++ 404.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Transparent Header Navigation bar Using HTML & CSS</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;700&display=swap" rel="stylesheet">
+  <style type="text/css">
+    * {
+      margin: 0;
+    }
+
+    #jumbotronCover {
+      background-attachment: static;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      height: 100vh;
+    }
+  </style>
+
+
+</head>
+
+<body>
+
+
+  <div class="row">
+    <div class="" style="height: 60px; background-color: black;"></div>
+    <div class="col-12" style="background-color: black;">
+      <h2></h2>
+      <div id="jumbotronCover" class="jumbotron bg-cover">
+        <div class="container text-white">
+          <h1 class="display-4">ERROR 404</h1>
+          <p class="lead">La page ne s'affiche pas, nous nous excusons de la gêne occasionnée et faisons tout notre
+            possible pour régulariser le problème.</p>
+          <hr class="my-4">
+          <p>Vous pouvez retourner sur la page d'accueil</p>
+          <a class="btn btn-primary btn-lg" href="index.php" role="button">Accueil</a>
+        </div>
+        <-- /.container -->
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+      integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
+      crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+      integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+      crossorigin="anonymous"></script>
+</body>
+
+</html>
